73c653603e956dbcdc2f52452d9f820cb8ed3653
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-01-08 15:16:48 -0800

    scene transition

--- game.gd
+++ game.gd
@@ -1,5 +1,12 @@
 extends Node
 
+@onready var obj_hold := $ObjectHolder
+@onready var curr_scene_hold := $CurrentScene
+@onready var transition := $Transition
+
+var curr_scene : Node
+
+signal scene_spawned(scene: String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,14 +14,37 @@ func _ready():
 
 enum SCENE_TRANSITION_TYPE {NORMAL, JUMP}
 
-func change_scene(scene: String, transition := SCENE_TRANSITION_TYPE.NORMAL) -> void:
-	pass
+func change_scene(scene: String, trans := SCENE_TRANSITION_TYPE.NORMAL) -> void:
+	match trans:
+		SCENE_TRANSITION_TYPE.NORMAL:
+			transition.transition("out")
+			await transition.trans_anim_done
+		SCENE_TRANSITION_TYPE.JUMP:
+			# I mean yeah, of course you do nothing, it's a jump cut
+			pass
+	spawn_scene(scene)
+	await scene_spawned
+	match trans:
+		SCENE_TRANSITION_TYPE.NORMAL:
+			transition.transition("in")
+			await transition.trans_anim_done
+		SCENE_TRANSITION_TYPE.JUMP:
+			# I mean yeah, of course you do nothing, it's a jump cut
+			pass
 
 
 func spawn_scene(scene: String) -> void:
 	var scn = load(scene).instance()
 	if scn.has_signal("done"):
 		scn.done.connect(handle_scene_done.bind(scn.scn_done_data))
+	
+	if curr_scene_hold.get_child_count() > 0:
+		for child in curr_scene_hold.get_children():
+			child.queue_free()
+	
+	curr_scene_hold.add_child(scn)
+	
+	emit_signal("scene_spawned", scene)
 
 
 func handle_scene_done(info: Dictionary) -> void:
---
+++ scenes/game/transition/transition.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@onready var anim_player := $AnimationPlayer
+
+signal trans_anim_done
+
+func _ready():
+	anim_player.play("RESET")
+
+
+func transition(dir: String) -> void:
+	match dir:
+		"in":
+			anim_player.play("trans_in")
+		"out":
+			anim_player.play("trans_out")
+	await anim_player.animation_finished
+	emit_signal("trans_anim_done")
